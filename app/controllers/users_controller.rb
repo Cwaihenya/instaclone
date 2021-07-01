2e8f219ef48a75aed074d0953e268f021122a4dc
Cwaihenya <cwaihenya.gmail.com>
2021-07-01 11:32:07 +0900

    prelim mailer set up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,6 +64,7 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email, :avatar)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation, :avatar)
     end
 end
