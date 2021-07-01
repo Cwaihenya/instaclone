f7cc05531cf951fc252142325f04d8a90df5c19b
Cwaihenya <cwaihenya.gmail.com>
2021-07-01 11:05:47 +0900

    basic usr set up

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+mount_uploader :avatar, AvatarUploader
+validates_integrity_of  :avatar
+  validates_processing_of :avatar
+
+  private
+    def avatar_size_validation
+      errors[:avatar] << "should be less than 500KB" if avatar.size > 0.5.megabytes
+    end
+end
