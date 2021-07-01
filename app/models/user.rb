697314567fb9663683332be96d4dc1d4cf6bb8cc
Cwaihenya <cwaihenya.gmail.com>
2021-07-01 11:07:15 +0900

    user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,12 @@ class User < ApplicationRecord
 mount_uploader :avatar, AvatarUploader
 validates_integrity_of  :avatar
   validates_processing_of :avatar
-
+  before_validation { email.downcase! }
+  validates :name,  presence: true, length: { maximum: 30 }
+   validates :email, presence: true, length: { maximum: 255 },
+                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+                     has_secure_password
+                       validates :password, presence: true, length: { minimum: 6 }
   private
     def avatar_size_validation
       errors[:avatar] << "should be less than 500KB" if avatar.size > 0.5.megabytes
