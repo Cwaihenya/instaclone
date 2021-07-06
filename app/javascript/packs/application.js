8ace5723a9faceab201078f772d525ef6fcf4efb
Cwaihenya <cwaihenya.gmail.com>
2021-07-06 09:49:29 +0900

    application css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,13 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
+import "./stylesheets/application.scss"
+import "popper.js"
+import "jquery"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+require('jquery')
+require( 'bootstrap')
